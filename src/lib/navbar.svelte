bb7faaa6f1df575ac1a1c7b4fbe34b128c18c154
Nehemia <66851037+Heracraft@users.noreply.github.com>
2023-02-05 19:16:33 +0300

    Updated nav url

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -7,7 +7,7 @@
 </script>
 <nav class="bg-white shadow-sm border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-zinc-800 z-10 dark:opacity-100 relative ">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="https://flowbite.com/" class="flex items-center">
+      <a href="/" class="flex items-center">
           <span class="self-center text-xl  whitespace-nowrap dark:text-white font-shadows">Nehemiahkaaya</span>
       </a>
       <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false"
